fed28643598cf5b63fca5020fcda472a9a676f75
andreister <andreister@f8c5e32e-89c0-11de-91c6-75d7f44f5486>
2009-08-15 18:06:46 +0000

    Initial implementation

---
+++ WhySharper/WhySharperDaemon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using JetBrains.DocumentModel;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Daemon;
+using JetBrains.ReSharper.Daemon.Impl;
+
+namespace WhySharper
+{
+    public class WhySharperDaemon : DaemonProcessBase
+    {
+        public WhySharperDaemon(IProjectFile projectFile) : base(projectFile) { }
+        public WhySharperDaemon(IProjectFile projectFile, IList<IDaemonStage> stages) : base(projectFile, stages) { }
+        public override bool IsRangeInvalidated(DocumentRange range) { return false; }
+        public override bool FullRehighlightingRequired { get { return true; } }
+
+        private readonly List<HighlightingInfo> _highlightings = new List<HighlightingInfo>();
+        public List<HighlightingInfo> Highlightings
+        {
+            get { return _highlightings; }
+        }
+
+        public void DoHighlighting()
+        {
+            DoHighlighting(DaemonProcessKind.VISIBLE_DOCUMENT, Commiter);
+        }
+
+        private void Commiter(DaemonCommitContext context)
+        {
+            _highlightings.AddRange(context.HighlightingsToAdd);
+        }
+    }
+}
