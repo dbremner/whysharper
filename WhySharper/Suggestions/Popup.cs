44ad6dca8ca042afa201782dec362486dfb95d14
andreister <andreister@f8c5e32e-89c0-11de-91c6-75d7f44f5486>
2009-08-19 01:01:46 +0000

    Add suggestions code

--- WhySharper/Suggestions/Popup.cs
+++ WhySharper/Suggestions/Popup.cs
@@ -1,19 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using JetBrains.CommonControls;
-using JetBrains.ReSharper.Daemon;
-using JetBrains.ReSharper.Daemon.CSharp.Stages;
 using JetBrains.UI.PopupMenu;
-using JetBrains.UI.RichText;
 
 namespace WhySharper.Suggestions
 {
+    /// <summary>
+    /// Displays WhySharper popup messages.
+    /// </summary>
     internal static class Popup
     {
-        private const string SubmitBugUrl = "http://code.google.com/p/whysharper/issues/list";
+        private static readonly SimpleMenuItem _askExplanationMenuItem = 
+            CreateMenuItem("...Have a worthwhile explantion? Let us know!", "http://code.google.com/p/whysharper/issues/list");
 
-        internal static void Display(string caption, List<SimpleMenuItem> menuItems)
+        /// <summary>
+        /// Displays a simple "ask for a suggestion" popup, the idea is that users would submit bugs
+        /// if they want to add a suggestion or more discussions to the existing suggestions. Then,
+        /// WhySharper contributors would update the xml that gets updated regulary (eg, on each VS startup).
+        /// </summary>
+        internal static void SubmitSuggestion() 
+        {
+            var menuItems = new List<SimpleMenuItem> { _askExplanationMenuItem };
+            Display("Ehm, no clue.", menuItems);
+        }
+
+        /// <summary>
+        /// Displays all links for the given suggestion.
+        /// </summary>
+        /// <param name="suggestion">Suggestion to display links for.</param>
+        internal static void Display(Suggestion suggestion)
+        {
+            var menuItems = suggestion.Links.ConvertAll(i => CreateMenuItem("\"" + i.Key + "\"", i.Value));
+            menuItems.Add(SimpleMenuItem.CreateSeparator());
+            menuItems.Add(_askExplanationMenuItem);
+
+            Display("How about this", menuItems);
+        }
+
+        private static void Display(string caption, List<SimpleMenuItem> menuItems)
         {
             var menu = new JetPopupMenu();
             menu.Caption.Value = WindowlessControl.Create(caption);
@@ -22,75 +46,15 @@ namespace WhySharper.Suggestions
             menu.Show();
         }
 
-        public static void Display(string caption, List<HighlightingInfo> highlightings)
+        private static SimpleMenuItem CreateMenuItem(string text, string urlOnClick) 
         {
-            var menuItems = highlightings.ConvertAll(h => CreateMenuItem(h));
-            Display(caption, menuItems);
-        }
+            const int maxLength = 80;
+            string tooltip = (urlOnClick.Length < maxLength) ? urlOnClick : urlOnClick.Substring(0, maxLength) + "...";
 
-        public static void Display(string caption, string singleItem)
-        {
-            Display(caption, singleItem, null);
-        }
+            var item = new SimpleMenuItem { Text = text, Style = MenuItemStyle.Enabled, Tooltip = tooltip };
+            item.Clicked += delegate { Process.Start(urlOnClick); };
 
-        private static void Display(string caption, string singleItem, EventHandler onClick)
-        {
-            var item = new SimpleMenuItem { Text = singleItem };
-            if (onClick != null)
-            {
-                item.Style = MenuItemStyle.Enabled;
-                item.Clicked += onClick;
-            }
-            Display(caption, new List<SimpleMenuItem> { item });
-        }
-
-        private static SimpleMenuItem CreateMenuItem(HighlightingInfo info)
-        {
-            string problemName = info.Highlighting.GetType().Name;
-
-            var result = new SimpleMenuItem {
-                Text = info.Highlighting.ErrorStripeToolTip,
-                Style = MenuItemStyle.Enabled,
-                Tooltip = new RichText(problemName, TextStyle.Default)
-            };
-            result.Clicked += delegate { RedirectToExplanation(problemName); };
-
-            return result;
-        }
-
-        private static void RedirectToExplanation(string problemName)
-        {
-            string url = GetExplanationUrl(problemName);
-            if (url != string.Empty) {
-                Process.Start(url);
-            }
-            else {
-                const string text = "Do you want to create a bug so that we can find an explanation and link to it?";
-                Display("No explanation to this so far.", text, delegate { Process.Start(SubmitBugUrl); });
-            }
-        }
-
-        /// <summary>
-        /// Find the explanation URL from given R# warning type name.
-        /// (Might be later put in a settings page, with an option to update.)
-        /// </summary>
-        /// <param name="name">Resharper warning/suggestion name.</param>
-        /// <returns></returns>
-        private static string GetExplanationUrl(string name)
-        {
-            if (name == typeof(MemberCanBeMadeStaticLocalWarning).Name)
-            {
-                return "http://stackoverflow.com/questions/169378/c-method-can-be-made-static-but-should-it";
-            }
-            else if (name == typeof(ConvertToConstantLocalWarning).Name)
-            {
-                return "http://stackoverflow.com/questions/909681/resharper-always-suggesting-me-to-make-const-string-instead-of-string";
-            }
-            else if (name == typeof(UnusedUsingDirectiveWarning).Name)
-            {
-                return "http://stackoverflow.com/questions/235250/what-are-the-benefits-of-maintaining-a-clean-list-of-using-directives-in-c";
-            }
-            return string.Empty;
+            return item;
         }
     }
 }
